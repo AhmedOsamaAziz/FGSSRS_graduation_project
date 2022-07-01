3fbaa5ecbbd875bf1d5a519b62c4df7b35f811f4
Ahmed_Madkour <ahmedfe1@hotmail.com>
2022-07-02 01:49:02 +0200

    updat app two

--- applications/admin.py
+++ applications/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Cycle,Stage,Cycle_Stage_Link,Documents,Application
 
 # Register your models here.
+admin.site.register(Cycle)
+admin.site.register(Stage)
+admin.site.register(Cycle_Stage_Link)
+admin.site.register(Documents)
+admin.site.register(Application)
--- applications/models.py
+++ applications/models.py
@@ -7,15 +7,34 @@ from generic.models import Person
 class Cycle(models.Model):
     cycle_name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.cycle_name
+
 class Stage(models.Model):
     stage_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.stage_name
+
 
 class Cycle_Stage_Link(models.Model):
     cycle_id = models.ForeignKey(Cycle,on_delete=models.PROTECT,related_name='application')
     stage_id = models.ForeignKey(Stage,on_delete=models.PROTECT,related_name='application')
 
+class Documents(models.Model):
+    name = models.CharField(max_length=100)
+    tabel_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 
 class Application(models.Model):
-    person_id = models.ForeignKey(Person,on_delete=models.PROTECT,related_name='application')
+    
     current_statge_link_id = models.ForeignKey(Cycle_Stage_Link,on_delete=models.PROTECT,related_name='application')
+    current_employee_id = models.ForeignKey(Person,on_delete=models.PROTECT,related_name='application')
+    #student_id = models.ForeignKey(Person,on_delete=models.PROTECT,related_name='application')
+    document_id = models.ForeignKey(Documents,on_delete=models.PROTECT,related_name='application')
+
 
